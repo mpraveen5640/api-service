e2166ae2a083f30f3d5184582454e3f0eb3a3ff8
Praveen <mpraveen5640@gmail.com>
2025-09-12 08:34:55 +0530

    demo class updated

--- src/main/java/com/example/api_service/constants/DemoClass.java
+++ src/main/java/com/example/api_service/constants/DemoClass.java
@@ -1,4 +1,11 @@
 package com.example.api_service.constants;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class DemoClass {
+    public static void main(String[] args) {
+        List<Integer> input = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        input.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+    }
 }
