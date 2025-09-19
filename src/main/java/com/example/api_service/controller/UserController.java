9e0cc902b4daf09e18e87f173986d513fe6b16d5
Praveen <mpraveen5640@gmail.com>
2025-09-19 08:17:45 +0530

    logger updated

--- src/main/java/com/example/api_service/controller/UserController.java
+++ src/main/java/com/example/api_service/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.api_service.controller;
 import com.example.api_service.entity.UserEntity;
 import com.example.api_service.exception.UserNotFoundException;
 import com.example.api_service.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,8 @@ import java.util.List;
 @RequestMapping("/api")
 @RestController
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Value("${app.name}")
     private String appName;
@@ -28,7 +32,9 @@ public class UserController {
 
     @PostMapping("create/user")
     public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity userEntity) {
-        return new ResponseEntity<>(userService.createUser(userEntity), HttpStatus.CREATED);
+        UserEntity userEntityResponse = userService.createUser(userEntity);
+        logger.info("Newly created user response: {}", userEntityResponse);
+        return new ResponseEntity<>(userEntityResponse, HttpStatus.CREATED);
     }
 
     @GetMapping("all/users")
